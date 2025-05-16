d8c3c57dca2e5c34c48dabdbf16dbd5a7f8d9ad9
I-Am-Shimanto <shimantowebdeveloper@gmail.com>
2025-05-17 02:07:10 +0600

    check

--- src/utils/Mainbtn.jsx
+++ src/utils/Mainbtn.jsx
@@ -6,7 +6,7 @@ const Mainbtn = ({ title }) => {
     <>
       <Link
         className="py-3 px-8 hover:bg-[#7289DA] hover:text-white ease-in-out duration-300 border border-[#7289DA] rounded-lg text-lg font-inter font-semibold text-[#7289DA]"
-        to="/"
+        to="/login"
       >
         {title}
       </Link>
