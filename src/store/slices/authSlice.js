d9bd3905bfcf02487694cac809a1edf265b6a353
iamShimanto <shimantowebdeveloper@gmail.com>
2025-06-17 22:37:12 -0700

    add redux

---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
